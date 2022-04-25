7d64f992cd65b02da4a7ee2981fcebb7512bc4e7
Fabian Fernandez <fdzfabian@gmail.com>
2022-04-25 01:21:24 -0600

    Add identity to Cliente table

--- dbo/Tables/Cliente.sql
+++ dbo/Tables/Cliente.sql
@@ -1,17 +1,14 @@
 ﻿CREATE TABLE [dbo].[Cliente] (
-    [Id]              INT           NOT NULL,
+    [Id]              INT           NOT NULL IDENTITY(1,1),
     [Nombre]          VARCHAR (64)  NOT NULL,
     [PrimerApellido]  VARCHAR (64)  NOT NULL,
     [SegundoApellido] VARCHAR (64)  NOT NULL,
-    [Correo]          VARCHAR (256) NOT NULL,
+    [Correo]          VARCHAR (256) NOT NULL UNIQUE,
     [Telefono]        VARCHAR (16)  NOT NULL,
-    [Usuario]         VARCHAR (16)  NOT NULL,
-    [Pass]            VARCHAR (16)  NOT NULL,
     [Direccion]       VARCHAR (512) NOT NULL,
     [Provincia]       VARCHAR (32)  NOT NULL,
     [Canton]          VARCHAR (64)  NOT NULL,
     [Distrito]        VARCHAR (64)  NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    UNIQUE NONCLUSTERED ([Usuario] ASC)
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
